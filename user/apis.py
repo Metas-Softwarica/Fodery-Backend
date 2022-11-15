111fb5f2c0ffa3ec70140c99713f054815b54432
suzanpradhan <Mrnischalpuri@gmail.com>
2022-11-15 21:24:16 +0545

    FOD-1 Google Sign In Api

--- user/apis.py
+++ user/apis.py
@@ -1,14 +1,22 @@
-from django.contrib.auth import get_user_model
+import os, requests, json
 from django.db import transaction
+from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth import login
+from django.http.response import (
+    HttpResponse,
+    HttpResponseBadRequest
+)
 
+import google_apis_oauth
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from .models import User, Profile
 from .serializers import RegistrationSerializer
-
-User = get_user_model()
+from services import token
+from fodery import settings
 
 
 class RegistrationView(generics.CreateAPIView):
@@ -22,11 +30,53 @@ class RegistrationView(generics.CreateAPIView):
             serializer.is_valid(raise_exception=True)
             user = serializer.save()
 
-            return Response(
-                {
-                    "access_token": str(
-                        RefreshToken.for_user(user).access_token
-                    )
-                },
-                status=201,
-            )
+
+
+# The url where the google oauth should redirect after a successful login.
+REDIRECT_URI = 'http://backend.sweedapp.com/user/google/callback/'
+# REDIRECT_URI = 'exp://192.168.1.73:19000'
+
+# Authorization scopes required
+SCOPES = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email', "openid"]
+
+JSON_FILEPATH = os.path.join(os.getcwd(), 'client9.json')
+
+def RedirectGoogleSignin(request):
+    oauth_url = google_apis_oauth.get_authorization_url(
+        JSON_FILEPATH, SCOPES, REDIRECT_URI)
+    return HttpResponseRedirect(oauth_url)
+
+def CallbackGoogleSignin(request):
+    try:
+        credentials = google_apis_oauth.get_crendentials_from_callback(
+            request,
+            JSON_FILEPATH,
+            SCOPES,
+            REDIRECT_URI
+        )
+        stringified_token = google_apis_oauth.stringify_credentials(
+            credentials)
+        stringified_token = json.loads(stringified_token)
+        response = requests.get(f"https://www.googleapis.com/oauth2/v3/userinfo?access_token={stringified_token['token']}")
+        jsonData = response.json()
+        print(jsonData)
+        if jsonData['email']:
+            userData = {
+                "first_name":jsonData['given_name'],
+                "last_name":jsonData['family_name']
+            }
+            user, userExist = User.objects.get_or_create(email=jsonData["email"])
+            profile.__dict__.update(userData)
+            user.save()
+            profile, _ = Profile.objects.get_or_create(user=user)
+            profile.save()
+            user.backend = settings.AUTH_PASSWORD_VALIDATORS[0]['NAME']
+            login(request, user)
+            # HttpResponseRedirect.allowed_schemes.append('sweed')
+            token_payload = token.get_tokens_for_user(user=user)
+            print(token_payload)
+            # return HttpResponseRedirect(f"sweed://?access_token={access_token}&refresh_token={refresh_token}")
+            return HttpResponse(token)
+        return HttpResponseBadRequest("Login Failed")
+    except google_apis_oauth.InvalidLoginException:
+        return HttpResponseBadRequest("Login Failed")
