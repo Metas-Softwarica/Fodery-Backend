dd6a614f8f2555e59653d5291d70064d2f744621
suzanpradhan <74817169+suzanpradhan@users.noreply.github.com>
2022-12-03 13:51:43 +0545

    Food Variant and Attribute Model Update

--- src/food/admin.py
+++ src/food/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Food, FoodType, Diet
+from .models import Food, FoodType, Diet, VariantPair, AttributeCollection, AttributeCollectionReferencePivot, FoodInvetory, AttributeReference, VariantPairFoodInventoryPivot
 # Register your models here.
 
 admin.site.register(Food)
 admin.site.register(FoodType)
 admin.site.register(Diet)
+admin.site.register(VariantPair)
+admin.site.register(AttributeCollection)
+admin.site.register(AttributeCollectionReferencePivot)
+admin.site.register(FoodInvetory)
+admin.site.register(AttributeReference)
+admin.site.register(VariantPairFoodInventoryPivot)
--- src/food/models.py
+++ src/food/models.py
@@ -1,4 +1,7 @@
+from decimal import Decimal
+
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Diet(models.Model):
@@ -33,3 +36,107 @@ class Food(models.Model):
     diets = models.ManyToManyField(Diet, blank=True)
     foodTypes = models.ManyToManyField(FoodType, blank=True)
     price = models.FloatField(null=True, blank=True, default=0)
+
+
+class FoodInvetory(models.Model):
+    sku = models.CharField(
+        max_length=20,
+        unique=True,
+    )
+    food = models.ForeignKey(
+        Food, related_name="food_food_inventory", on_delete=models.PROTECT)
+    attribute_collection = models.ForeignKey(
+        "AttributeCollection", related_name="attribute_collection_product_inventory", on_delete=models.PROTECT, blank=True, null=True,)
+    attributes = models.ManyToManyField(
+        "VariantPair",
+        related_name="variant_pair_product_inventory",
+        through="VariantPairFoodInventoryPivot",
+    )
+    is_default = models.BooleanField(
+        default=False,
+    )
+    retail_price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal("0.01"))]
+    )
+    store_price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def __str__(self):
+        return self.sku
+
+
+class AttributeReference(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AttributeCollection(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    attribute_references = models.ManyToManyField(
+        AttributeReference, related_name="attribute_collection_attribute_reference", through="AttributeCollectionReferencePivot")
+
+    def __str__(self):
+        return self.name
+
+
+class AttributeCollectionReferencePivot(models.Model):
+    """
+        Pivot Table between Attribute Colleciton and Attribute Reference
+    """
+    attribute_reference = models.ForeignKey(
+        AttributeReference,
+        related_name="attribute_reference_attribute_collection_reference_pivot",
+        on_delete=models.PROTECT,
+    )
+    attribute_collection = models.ForeignKey(
+        AttributeCollection,
+        related_name="attribute_collection_attribute_collection_reference_pivot",
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        unique_together = (("attribute_reference",
+                           "attribute_collection"),)
+
+
+class VariantPair(models.Model):
+    reference = models.ForeignKey(
+        AttributeReference, related_name="attribute_attribute_reference", on_delete=models.PROTECT)
+    value = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.reference.name} : {self.value}"   
+
+
+class VariantPairFoodInventoryPivot(models.Model):
+    """
+        Pivot Table between Attribute Pai and Food Inventory
+    """
+    attribute_pair = models.ForeignKey(
+        VariantPair,
+        related_name="variant_pair_variant_pair_food_inventory_pivot",
+        on_delete=models.PROTECT,
+    )
+    food_inventory = models.ForeignKey(
+        FoodInvetory,
+        related_name="food_inventory_variant_pair_food_inventory_pivot",
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        unique_together = (("attribute_pair", "food_inventory"),)
